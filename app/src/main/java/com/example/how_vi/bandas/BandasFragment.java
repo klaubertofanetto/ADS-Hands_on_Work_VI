8ffbbe7f27ec09577a0c6b3f1acd949dd32b0e70
klaubertofanetto <klaubertofanetto@gmail.com>
2022-06-27 20:06:56 -0300

    commit inicial - menu e listar coleção

---
+++ app/src/main/java/com/example/how_vi/bandas/BandasFragment.java
@@ -0,0 +1,31 @@
+package com.example.how_vi.bandas;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.how_vi.R;
+
+public class BandasFragment extends Fragment {
+
+    public BandasFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_bandas, container, false);
+    }
+}
---
+++ app/src/main/java/com/example/how_vi/discos/DiscosFragment.java
@@ -0,0 +1,32 @@
+package com.example.how_vi.discos;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.how_vi.R;
+
+public class DiscosFragment extends Fragment {
+
+
+    public DiscosFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_discos, container, false);
+    }
+}
